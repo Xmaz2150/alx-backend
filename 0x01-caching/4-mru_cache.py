618c517739ddf5938e30996dd3bd64ebeb281115
tAf <muzirat0@gmail.com>
2024-07-26 23:24:47 +0200

    MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+basic_cache module
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """
+    MRUCache defines:
+    - derived class from BaseCachin
+    - MRU cache
+    """
+
+    def __init__(self):
+        """
+        initializer
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Add an item to cache using MRU
+        """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data.move_to_end(key)
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > self.MAX_ITEMS:
+                t_key = self.cache_data.popitem(last=True)[0]
+                print('DISCARD: {}'.format(t_key))
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key)
+            return self.cache_data.get(key)
+        return None
