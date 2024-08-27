7baf511dec6b690739ca30a9c0d4187e6ec811b3
tAf <muzirat0@gmail.com>
2024-08-27 18:02:15 +0200

    3. Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import { createClient }  from 'redis';
+
+const client = createClient();
+
+const connectToRedis = async () => {
+  client.on('connect', () => {
+    console.log('Redis client connected to the server', () => {
+      displaySchoolValue('Holberton');
+      setNewSchool('HolbertonSanFrancisco', '100');
+      displaySchoolValue('HolbertonSanFrancisco');
+    });
+  });
+  client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error}`);
+  });
+};
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, client.print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+  const val = await client.get(schoolName);
+  console.log(val);
+};
+
+connectToRedis();
